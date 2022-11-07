01307d8571dde6f6642a1172acf4c8198f03d6e0
Binh <teamgf2002@gmail.com>
2022-11-07 12:19:22 +0700

    frist commit

---
+++ src/inventorysys/InventorySys.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inventorysys;
+
+
+
+/**
+ *
+ * @author hi
+ */
+public class InventorySys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     new home().setVisible(true);
+        
+       
+    }
+    
+    
+}
